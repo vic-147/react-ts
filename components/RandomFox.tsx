9bbc2774c006feb2a7e711388e730999fe98c6a6
vic-147 <dmkorps@gmail.com>
2023-05-27 20:07:08 -0400

    :zap: Tipos

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,8 +1,12 @@
-// random number between 1 and 123
-const random = (): number => Math.floor(Math.random() * 123) + 1
-
-export const RandomFox = (): JSX.Element => {
-  const image: string = `https://randomfox.ca/images/${random()}.jpg`;
-
-  return <img width={320} height="auto" src={image} alt="image" className="rounded-lg" />
-}
+type Props = { image: string };
+export const RandomFox = ({ image }: Props): JSX.Element => {
+  return (
+    <img
+      width={320}
+      height="auto"
+      src={image}
+      alt="image"
+      className="rounded-lg"
+    />
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,29 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { RandomFox } from '@/components/RandomFox'
+import { useState } from "react";
+import { Inter } from "next/font/google";
+import { RandomFox } from "@/components/RandomFox";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+const generateId = () => Math.random().toString(36).substring(2, 9);
+const random = (): number => Math.floor(Math.random() * 123) + 1;
+
+type ImageItem = { id: string; url: string };
 
 export default function Home() {
+  const [images, setImages] = useState<Array<ImageItem>>([
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+  ]);
+
   return (
-    <main
-      className={`flex justify-center items-center ${inter.className}`}
-    >
-      <RandomFox />
+    <main className={`flex justify-center items-center ${inter.className}`}>
+      {images.map(({ id, url }) => (
+        <div className="p-4" key={id}>
+          <RandomFox image={url} />
+        </div>
+      ))}
     </main>
-  )
+  );
 }
